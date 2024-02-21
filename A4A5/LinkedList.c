896b6135c94805dab2e6c541d61f732bd85d7fda
wylliefang <fang.wenxu@northeastern.edu>
2024-02-20 19:21:06 -0800

    add LinkedList.c

--- A4A5/LinkedList.c
+++ A4A5/LinkedList.c
@@ -8,6 +8,15 @@ LinkedListNode* CreateLinkedListNode(char *data){
     if(nodeP == NULL){return NULL;}
     else{
         nodeP->payload=data;
+        /*
+        nodeP->payload = (char *)malloc(sizeof(char)*(strlen(data)+1));
+        if (nodeP->payload == NULL) {
+            //remember to free
+            free(nodeP);
+            return NULL;
+        }
+        strcpy(nodeP->payload, data);
+        */
         nodeP->prev = NULL;
         nodeP->next = NULL;
         return nodeP;
@@ -16,8 +25,11 @@ LinkedListNode* CreateLinkedListNode(char *data){
 
 int DestroyLinkedListNode(LinkedListNode *node){
     if(node == NULL){return 1;}
+    // if(node->payload != NULL){
+    //     free(node->payload);
+    // }
     /*
-    free(node->payload);
+    free(node->payload);  //necessary
     free(node->prev);
     free(node->next);
     node->payload = NULL;
@@ -28,22 +40,33 @@ int DestroyLinkedListNode(LinkedListNode *node){
 }
 
 int RemoveHead(LinkedList list){
-    if(list == NULL || list->head == NULL){ return 1;}
+    if(list == NULL || list->head == NULL){return 1;}
     else{
-        LinkedListNodePtr toRemove = list->head;
-        if(list->head->next == NULL){
+        LinkedListNodePtr toRemove = list->head; 
+        LinkedListNodePtr toRemoveNext = list->head->next;
+        //free(toRemove);  //F
+        if (DestroyLinkedListNode(toRemove) == 1) {
+            return 1;
+        }
+
+        if(list->num_elements == 1){
+            // //check
+            // if (DestroyLinkedList(list) == 1) {
+            //     return 1;
+            // }
             list->head = NULL;
             list->tail = NULL;
-            DestroyLinkedList(list);
+        } else {
+            // list->head = list->head->next;
+            // list->head->prev = NULL;
+            // if(list->head->next == NULL){
+            //     list->tail = list->head;
+            // }
+            list->head = toRemoveNext;
+            toRemoveNext->prev = NULL;
         }
-        else{
-            list->head = list->head->next;
-            list->head->prev = NULL;
-            if(list->head->next == NULL){
-                list->tail = list->head;
-            }
-        }
-        free(toRemove);
+
+        list->num_elements--;
         return 0;
     }  
 }
@@ -52,19 +75,41 @@ int RemoveTail(LinkedList list){
     if(list == NULL || list->head == NULL || list->tail == NULL){ return 1;}
     else{   
         LinkedListNodePtr toRemove = list->tail;
-        if(list->tail->prev == NULL){
+        LinkedListNodePtr toRemovePrev = list->tail->prev;
+
+        printf("------\n");
+        for (LinkedListNodePtr ptr = list->head; ptr != NULL; ptr = ptr->next) {
+            printf("%p %p %p\n", ptr->prev, ptr, ptr->next);
+        }
+        printf("------\n");
+        printf("checkpoint1\n");
+        if(DestroyLinkedListNode(toRemove) == 1){
+            return 1;
+        }
+        printf("checkpoint2\n");
+        // free(toRemove);
+        if(list->num_elements == 1){
             list->head = NULL;
             list->tail = NULL;
-            DestroyLinkedList(list);
+            printf("checkpoint3\n");
         }
         else{
-            list->tail = list->tail->prev;
-            list->tail->next = NULL;
-            if(list->tail->prev == NULL){
-                list->head = list->tail;
-            }
+            // list->tail = list->tail->prev;
+            // list->tail->next = NULL;
+            // if(list->tail->prev == NULL){
+            //     list->head = list->tail;
+            // }
+            printf("checkpoint4\n");
+            list->tail = toRemovePrev;
+            printf("checkpoint6\n");
+            printf("toRemovePrev: %p\n", toRemovePrev);
+            if(toRemovePrev->next !=NULL)
+            toRemovePrev->next = NULL;
+            printf("checkpoint5\n");
         }
-        free(toRemove);
+        //DestroyLinkedListNode(toRemove);
+        list->num_elements--;
+        printf("END\n");
         return 0;
     }
 }
@@ -72,15 +117,33 @@ int RemoveTail(LinkedList list){
 int RemoveLLElem(LinkedList list, LinkedListNodePtr ptr){
     if(list == NULL || ptr == NULL){ return 1;}
 
-    if(ptr == list->head){ return RemoveHead(list);} 
+    printf("head: %p\n", list->head);
+    printf("tail: %p\n", list->tail);
+    printf("element: %ld\n", list->num_elements);
+    printf("ptr: %p\n", ptr);
+
+    if(ptr == list->head){ 
+        printf("Remove HEAD\n");
+        return RemoveHead(list);
+    } 
     else if (ptr == list->tail){
+        printf("Remove Tail\n");
         return RemoveTail(list);
     }
     else{
+        printf("OTHER\n");
         LinkedListNodePtr toRemove = ptr;
-        ptr->prev->next = toRemove->next;
-        ptr->next->prev = toRemove->prev;
-        free(toRemove);
+        printf("\n\n%p\t%p\t%p\n\n",ptr->prev, ptr, ptr->next);
+        if(ptr->prev != NULL && ptr->next != NULL){
+            ptr->prev->next = toRemove->next;
+            ptr->next->prev = toRemove->prev;
+        } else if (ptr->prev == NULL && ptr->next == NULL) {
+            list->head = NULL;
+            list->tail = NULL;
+        }
+        
+        // DestroyLinkedListNode(toRemove);
+        list->num_elements--;
     }
     return 0;
     
@@ -98,12 +161,18 @@ int DestroyLinkedList(LinkedList list){
     if(list == NULL || list->head == NULL) {return 1;}
 
     LinkedListNode* nodeP = list->head;
+    printf("=====\n");
     while(nodeP != NULL){
+        printf("%p\n", nodeP);
         LinkedListNode* toRemove = nodeP;
         nodeP = nodeP->next;
-        DestroyLinkedListNode(toRemove);
-        return 0;
+        if (DestroyLinkedListNode(toRemove) == 1) {
+            return 1;
+        }
+        // return 0;
     }
+    free(list);
+
     return 0;
 }
 
@@ -114,24 +183,38 @@ unsigned int NumElementsInLinkedList(LinkedList list){
 int InsertLinkedList(LinkedList list, char* data){
     if(list == NULL || data == NULL){return 1;}
     LinkedListNode* nodeP = CreateLinkedListNode(data);
-    nodeP->next = list->head->next;
+    //forgot
+    if (list->num_elements >= 1) {
+        list->head->prev = nodeP;
+    }
+    nodeP->next = list->head;
     list->head = nodeP;
+    //add tail
+    if(list->num_elements == 0){
+        list->tail = nodeP;
+    }
     list->num_elements++;
     return 0;
 }
 
 int PopLinkedList(LinkedList list, char** dataPtr){
     if (list == NULL || list->head == NULL || dataPtr == NULL) { return 1; }
-    *dataPtr = list->head->payload;
+    
+    *dataPtr = (char*)malloc(sizeof(char)*(strlen(list->head->payload)+1));
+    if (*dataPtr == NULL) {
+        return 1;
+    }
+    strcpy(*dataPtr, list->head->payload);
+    // *dataPtr = list->head->payload;
     LinkedListNodePtr toRemove = list->head;
-    if (list->head == list->tail) { // Single element
+    if (list->head == list->tail) { 
         list->head = NULL;
         list->tail = NULL;
     } else {
         list->head = list->head->next;
         list->head->prev = NULL;
     }
-    free(toRemove); // Adjust to proper node deallocation if needed
+    // DestroyLinkedListNode(toRemove); 
     list->num_elements--;
     return 0;
 }
